294d4afc02c6b0c0d12436e7a96dbd788e55e619
bensouici akram <akram.bensouici@forage.ai>
2023-07-27 20:29:56 +0100

    changing the log level

--- scraper.py
+++ scraper.py
@@ -158,7 +158,6 @@ if __name__ == '__main__' :
             'USER_AGENT':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
             'FEED_URI':'output.csv',
             'FEED_FORMAT':'csv',
-            'LOG_LEVEL':'ERROR'
         }
     )
     process.crawl(InfosSpider)
